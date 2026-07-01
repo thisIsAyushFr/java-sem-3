0c2b3aa4d6478a26c7a6f2cdece1823d2e44d64a
Calm-ayush <ayushrollno5@gmail.com>
2026-07-01 09:58:56 +0530

    new

--- class2/class2b.java
+++ class2/class2b.java
@@ -1,18 +1,8 @@
 package class2;
 
-public class class2a {
+public class class2b {
     public static void main(String[] args){
         int n=5;
-        for(int i=1;i<=n;i++){
-            for(int j=1;j<=i;j++){
-                System.out.print(i+" ");
-            }
-            System.out.println();
-        }
-
-
-        System.out.println("next");
-
         for(int i=n;i>=0;i--){
             for(int j=1;j<=i;j++){
                 System.out.print(i+" ");
