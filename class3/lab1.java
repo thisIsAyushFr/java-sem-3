766bca8d6a7ba8949358fb0d16acea60f75c6a1d
Calm-ayush <ayushrollno5@gmail.com>
2026-07-14 23:16:38 +0530

    new

---
+++ class3/lab1.java
@@ -0,0 +1,11 @@
+public class lab1 {
+            public int i;
+
+    public static void main(String[] args) {
+        System.out.println("Hellow World");
+        lab1 obj = new lab1();
+        obj.i = 5;
+        System.out.println("value = "+obj.i);
+    }
+    
+}
