1cc7f6ebd34398a02429a9d5622ae00410947309
Calm-ayush <ayushrollno5@gmail.com>
2026-08-17 11:57:21 +0530

    new

--- class15/interfacevsasbtract.java
+++ class15/interfacevsasbtract.java
@@ -4,11 +4,17 @@ public class interfacevsasbtract {
 
     interface shape{
         public void display();
-        //public void calculateArea();
+        public void calculateArea();
     }
      static class Square implements shape{
         public void display(){
             System.out.println("Hey i am a sqaure");
+        }
+        public void calculateArea(){
+            int area;
+            int side=5;
+            area=side*side;
+            System.out.println("Area of sqaure is " + area);
         }
     }
 
@@ -16,13 +22,22 @@ public class interfacevsasbtract {
     public void display(){
         System.out.println("Hey i am a rectangle");
     }
+    public void calculateArea(){
+            int area;
+            int len=5;
+            int breadth=3;
+            area=len*breadth; 
+            System.out.println("Area of rectangle is " + area);
+        }
 }
 public class Main{
     public static void main(String[] args) {
         shape obj = new Square();
         obj.display();
+        obj.calculateArea();
         obj = new rectangle();
         obj.display();
+        obj.calculateArea();
 
     }
 }
